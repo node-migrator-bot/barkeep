2b777122cc5eea0319a1c0941579e7255b73e718
Clay Smith <smithclay@gmail.com>
2012-06-08 14:49:25 -0700

    create grunt tasks from barkeep functions

---
+++ tasks/files.js
@@ -0,0 +1,41 @@
+var util = require('util'),
+    path = require('path'),
+    fs = require('fs'),
+    glob = require('glob');
+    
+module.exports = function(grunt) {    
+    // ## directory
+    // Creates a directory if one doesn't already exist, with optional callback.
+    grunt.registerHelper('directory', function (dir, callback) {
+        if (!dir) {
+            throw 'You must specify a directory name.';
+        }
+        
+        if (!path.existsSync(dir)){
+            fs.mkdirSync(dir);
+            if (callback){
+                callback(dir);
+            }
+        }    
+        return this;
+    });
+    
+    // # fileListSync
+    // From a glob pattern, gets a listing of files. Optionally excludes some files.
+   grunt.registerHelper('fileListSync', function (globPattern, excludes) {
+        var allFiles = glob.sync(globPattern);
+
+        if (allFiles.length === 0) {
+            return [];
+        }
+
+        if (excludes && excludes.length > 0) {
+            allFiles = allFiles.filter(function (file) {
+                return excludes.every(function (exclude) { 
+                        return file.indexOf(exclude) === -1;
+                    });
+            });
+        }
+        return allFiles;
+    });  
+};
