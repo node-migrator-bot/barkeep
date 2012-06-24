ef55a36d57ae85745f1b72d0c3c40d782329ee9f
smithclay <smithclay@gmail.com>
2012-06-24 15:20:44 -0700

    add gzip support for ducksboard size reporting

--- tasks/ducksboard.js
+++ tasks/ducksboard.js
@@ -9,24 +9,50 @@
 module.exports = function(grunt) {    
     var peking = require('peking');
     var fs = require('fs');
+    var gzip = require('gzip-js');
     
     // ## helper filesize
-    // Get the combined filesize of a collection of files.
+    // Get the combined filesize (including gzipped) of a collection of files.
     grunt.registerHelper('filesize', function (files, cb) {
+        // Keep track of the total numbe of bytes.
         var totalSize = 0;
+        // Store all of the text we read from each file.
+        var totalData;
         grunt.utils.async.forEach(files, function(file, callback) {
-            fs.stat(file, function(err, stat) {
-                if (err) {
-                    callback(err);
-                } 
-                totalSize += stat.size;
-                callback();                               
-            });
+            grunt.utils.async.parallel([
+                function(cb2) {
+                    fs.stat(file, function(err, stat) {
+                        if (err) {
+                            cb2(err);
+                        } 
+                        totalSize += stat.size;
+                        cb2();                               
+                    });                    
+                },
+                function(cb2) {
+                    fs.readFile(file, function(err, data) {
+                        if (err) {
+                            cb2(err);
+                        }
+                        totalData += data;
+                        cb2();
+                    })
+                }
+                ], function(err, results) {
+                    if (err) {
+                        callback(err);
+                    } 
+                    callback();                               
+                });
         }, function(err) {
             if (err) {
                 cb(err, null);
             }
-            cb(null, totalSize);
+            
+            // Get compressed size of all of the files.
+            var compressed = gzip.zip(totalData, {level: 6});
+            
+            cb(null, totalSize, compressed.length);
         });
     });
     
@@ -41,11 +67,11 @@ module.exports = function(grunt) {
         }
         
         var js = grunt.file.expandFiles(this.file.src);        
-        grunt.helper('filesize', js, function(err, totalSize) {
+        grunt.helper('filesize', js, function(err, totalSize, compressedSize) {
             if (err) {
                 grunt.warn(err);
             }
-            grunt.log.writeln('[' + task.target + '] total size (bytes): ' + totalSize);
+            grunt.log.writeln('[' + task.target + '] total size (bytes): ' + totalSize + ' data ' + compressedSize);
             peking.pushValue({value: totalSize, endpoint: endpoint, api_key: grunt.config('meta.ducksboard_api_key')}, function (err) {
                 if (err) {
                     grunt.warn('Could not send data to ducksboard. Code ' + err + '.');
